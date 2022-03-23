4dc3bdeba824e891a9375e029c754dc0f32b5251
Luís Soares <lsoares@newstore.com>
2022-03-23 22:40:24 +0000

    search in amazon

---
+++ cypress/integration/test-index.js
@@ -0,0 +1,8 @@
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        cy
+            .visit("https://amazon.com")
+            .get("#twotabsearchtextbox").type("kindle").type("{enter}")
+            .click()
+    })
+})
