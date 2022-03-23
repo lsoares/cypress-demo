70a0db82616a1bee8f505632510ca9bd75da175a
Luís Soares <lsoares@newstore.com>
2022-03-23 23:14:42 +0000

    fix test

--- cypress/integration/test-index.js
+++ cypress/integration/test-index.js
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
+describe('Amazon cart test', () => {
+    it('adds to cart', () => {
         cy
             .visit("https://amazon.com")
             .get("#twotabsearchtextbox").type("kindle").type("{enter}")
